32d138c281e76524d486b5dc5eb2d6c21342153a
Zongru Shao <zongru.shao@silicon-austria.com>
2023-01-03 06:54:33 +0100

    add download script for talkbank ivanova

---
+++ download-ivanova.sh
@@ -0,0 +1,14 @@
+mkdir Ivanova
+cd Ivanova
+mkdir AD
+cd AD
+wget --user=broca --password='wernicke'  -r -nd -A.mp3 https://media.talkbank.org/dementia/Spanish/Ivanova/AD/
+cd ..
+mkdir HC
+cd HC
+wget --user=broca --password='wernicke'  -r -nd -A.mp3 https://media.talkbank.org/dementia/Spanish/Ivanova/HC/
+cd ..
+mkdir MCI
+cd MCI
+wget --user=broca --password='wernicke'  -r -nd -A.mp3 https://media.talkbank.org/dementia/Spanish/Ivanova/MCI/
+cd ..
