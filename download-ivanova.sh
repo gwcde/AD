6e81ff8d41d87533d25e750449c2a14bf2293234
Zongru Shao <zongru.shao@silicon-austria.com>
2023-01-05 09:37:06 +0100

    fix wget download multiple times

--- download-ivanova.sh
+++ download-ivanova.sh
@@ -1,14 +1,1 @@
-mkdir Ivanova
-cd Ivanova
-mkdir AD
-cd AD
-wget --user=broca --password='wernicke'  -r -nd -A.mp3 https://media.talkbank.org/dementia/Spanish/Ivanova/AD/
-cd ..
-mkdir HC
-cd HC
-wget --user=broca --password='wernicke'  -r -nd -A.mp3 https://media.talkbank.org/dementia/Spanish/Ivanova/HC/
-cd ..
-mkdir MCI
-cd MCI
-wget --user=broca --password='wernicke'  -r -nd -A.mp3 https://media.talkbank.org/dementia/Spanish/Ivanova/MCI/
-cd ..
+wget --user=broca --password='wernicke' -r -np -k -A.mp3 https://media.talkbank.org/dementia/Spanish/Ivanova/
