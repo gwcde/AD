a2c4cffee03dfeecab35c9de5adc7d46c30180e2
gwcde <61725013+gwcde@users.noreply.github.com>
2023-02-03 19:22:20 +0800

    Delete S3prl-main directory

--- S3prl-main/find_content.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z $1 ]; then
-    echo "Usage: $0 <pattern>"
-    exit 1
-fi
-
-ag $1 $(cat valid_paths.txt)
--- S3prl-main/hubconf.py
+++
@@ -1,4 +0,0 @@
-from s3prl import hub
-
-for _option in hub.options():
-    globals()[_option] = getattr(hub, _option)
